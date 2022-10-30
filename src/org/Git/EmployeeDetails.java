8e03cd49a030a2512de656596d72de4a8a0ccd3a
malathiprakash <malathipriya.p@gmail.com>
2022-10-30 20:01:09 +0530

    my task

---
+++ src/org/Git/EmployeeDetails.java
@@ -0,0 +1,17 @@
+package org.Git;
+
+public class EmployeeDetails {
+	private void empname() {
+ System.out.println("Anand");
+	}
+	private void empmobile() {
+System.out.println("siva");
+	}
+
+	public static void main(String[] args) {
+		EmployeeDetails E=new EmployeeDetails();
+		E.empmobile();
+		E.empname();
+	}
+
+}
