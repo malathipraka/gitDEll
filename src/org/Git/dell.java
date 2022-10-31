20a7b9979b79feb2c085ef13f95d6f7c69efc76e
malathiprakash <malathipriya.p@gmail.com>
2022-10-31 06:41:07 +0530

    babu task

---
+++ src/org/Git/dell.java
@@ -0,0 +1,58 @@
+
+package org.Git;
+
+import org.testng.annotations.Test;
+
+public class dell {
+
+	public class task1 {
+
+		@Test
+		private void test1() {
+			System.out.println("testmethod test1");
+		}
+
+		@Test
+		private void test2() {
+			System.out.println("testmethod test2");
+		}
+
+		@Test
+		private void test3() {
+			System.out.println("testmethod test3");
+		}
+
+		@Test
+		private void test4() {
+			System.out.println("testmethod test4");
+		}
+
+		@Test
+		private void test5() {
+			System.out.println("testmethod test5");
+		}
+
+		@Test
+		private void newadd() {
+
+			System.out.println("new add");
+		}
+
+		@Test
+		private void testb() {
+			System.out.println("testmethod testb");
+		}
+
+		@Test
+		private void testc() {
+			System.out.println("testmethod testc");
+		}
+
+		@Test
+		private void testa() {
+			System.out.println("testmethod testa");
+		}
+
+	}
+
+}
